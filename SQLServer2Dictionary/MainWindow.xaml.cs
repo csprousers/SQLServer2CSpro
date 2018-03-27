252993126e5d2457bccb8bdc86efeb282a68758d
Josh Handley <josh@teleyah.com>
2018-03-26 20:53:52 -0400

    Add documents folder to dict path if none is specified

--- SQLServer2Dictionary/MainWindow.xaml.cs
+++ SQLServer2Dictionary/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Win32;
-using SqlConnectionDialog;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,17 +6,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace SQLServer2Dictionary
 {
@@ -275,6 +267,12 @@ namespace SQLServer2Dictionary
                 }
             }
 
+            // Add documents directory to path if none specified
+            if (!Path.IsPathRooted(DictionaryPath))
+            {
+                DictionaryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), DictionaryPath);
+            }
+
             try
             {
                 DictionaryGenerator generator = new DictionaryGenerator();
@@ -300,7 +298,8 @@ namespace SQLServer2Dictionary
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
             saveFileDialog.Filter = "CSPro Data Dictionary Files (*.dcf)|*.dcf";
-            saveFileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(DictionaryPath);
+            saveFileDialog.InitialDirectory = Path.GetDirectoryName(DictionaryPath);
+            saveFileDialog.FileName = DictionaryPath;
             if (saveFileDialog.ShowDialog() == true)
             {
                 DictionaryPath = saveFileDialog.FileName;
